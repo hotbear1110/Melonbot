018a1ba9cd737999319a0b217a961f02835abf83
TomNordesen <joachimflottorp@gmail.com>
2021-10-06 21:39:36 +0200

    Nice Commit! :)

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,15 @@
+const shell = require("child_process")
+const tools = require("../tools/tools")
+
 module.exports = {
     name: "ping",
     ping: true,
     execute: async (channel, user, input, perm) => {
         try {
-            return `pong FeelsOkayMan`;
+            const commitCount = shell.execSync("git rev-list --all --count")
+            const commitSha = shell.execSync("git rev-parse --short HEAD")
+            
+            return `pong FeelsOkayMan . Running for NaN hours. master, ${commitSha}, commit ${commitCount}`;
         } catch (err) {
             return `Madge , bad command, you almost killed me! Notifying owner FeelsWeirdMan`
         }
