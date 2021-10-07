43dce9f7c9e5a444beb8522a2c5923df48ddb465
TomNordesen <joachimflottorp@gmail.com>
2021-10-07 18:04:01 +0200

    Nice Commit! :)

--- commands/ping.js
+++ commands/ping.js
@@ -9,7 +9,7 @@ module.exports = {
             const commitCount = shell.execSync("git rev-list --all --count")
             const commitSha = shell.execSync("git rev-parse --short HEAD")
             
-            return `pong FeelsOkayMan . Running for NaN hours. master, ${commitSha}, commit ${commitCount}`;
+            return `pong FeelsOkayMan . Running for ${tools.humanizeDuration(process.uptime())}. master, ${commitSha}, commit ${commitCount}`;
         } catch (err) {
             return `Madge , bad command, you almost killed me! Notifying owner FeelsWeirdMan`
         }
