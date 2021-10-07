2baa6b55ec83631c94ea503d3959ce3cd251b7c1
TomNordesen <joachimflottorp@gmail.com>
2021-10-07 23:01:23 +0200

    Nice Commit! :)

--- init.js
+++ init.js
@@ -1,5 +1,5 @@
 // Main entry of the bot, init database and stuff.
-// ASDMAPSID
+
 // Init database
 require("./tools/tools").initDatabase();
 // Keep pinging database to keep the connection alive.
