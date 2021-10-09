adac51ed73c5b2759b2a8cf1c8699c792c01b919
TomNordesen <joachimflottorp@gmail.com>
2021-10-10 00:15:40 +0200

    Nice Commit! :)

--- tools/prefix.js
+++ tools/prefix.js
@@ -3,8 +3,22 @@ const creds = require("./../credentials/config")
 module.exports = {
     description: "Every prefix that should trigger the bot. Does not support user input... Yet ?",
     prefix: (channel, user, message, self) => [
-        {prefix: "melon", condition: `${creds.DEVELOPMENT} === false;`, description: "Can only be used if development is set to false. Primarily allows you to work on dev bot while not triggering the production bot."},
-        {prefix: "melondev", condition: `${creds.DEVELOPMENT};`, description: "Opposite of 'melon' prefix, only trigger the development bot."},
-        {prefix: "foo", condition: `${user['user-id']} === ${creds.OWNER_USER_ID}`, description: "Only work if command caller is owner of the bot."}
+        {
+            prefix: "melon", 
+            condition: `${creds.DEVELOPMENT} === false`, 
+            description: "Can only be used if development is set to false. Primarily allows you to work on dev bot while not triggering the production bot."
+        },
+        
+        {
+            prefix: "melondev", 
+            condition: `${creds.DEVELOPMENT}`, 
+            description: "Opposite of 'melon' prefix, only trigger the development bot."
+        },
+
+        {
+            prefix: "foo", 
+            condition: `${user['user-id']} === ${creds.OWNER_USER_ID}`, 
+            description: "Only work if command caller is owner of the bot."
+        }
     ],
 }
