599bed2b63570976249e3827da5c812642e72a71
TomNordesen <joachimflottorp@gmail.com>
2021-10-14 19:09:48 +0200

    Nice Commit! :)

--- bot.js
+++ bot.js
@@ -38,7 +38,7 @@ const socket = process.platform !== "win32" ? new UnixServer() : null;
         //     }
         // }
 
-        if ((channel === "#nymn" && creds.DEVELOPMENT === false) && (input[0].toLowerCase() === "forsen" || input[1].toLowerCase() === "forsen")) {
+        if ((channel === "#nymn" && creds.DEVELOPMENT === false) && (input[0].toLowerCase() === "forsen" || (input[0].toLowerCase() === "Nime" && input[1].toLowerCase() === "forsen"))) {
             client.say(channel, "Nime ❗ ")
             return
         }
