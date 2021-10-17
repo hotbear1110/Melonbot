0ff04d0f589666781d1c9e332201659fb05590ef
TomNordesen <joachimflottorp@gmail.com>
2021-10-17 18:36:36 +0200

    Nice Commit! :)

---
+++ commands/markov.js
@@ -0,0 +1,18 @@
+const tools = require("../tools/tools")
+const UnixServer = require("./../modules/socket")
+
+module.exports = {
+    name: "markov",
+    ping: false,
+    description: "Returns a sentence parsed from a markov bot on the channel.",
+    perm: 100,
+    execute: async (channel, user, input, perm) => {
+        try {
+            const socket = new UnixServer();
+            socket.connect();
+            return await socket.read(input.join(" "));
+        } catch (err) {
+            return `Madge , bad command, you almost killed me! Notifying owner FeelsWeirdMan ${err}`
+        }
+    }
+}
