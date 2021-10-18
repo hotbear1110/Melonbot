70db9165220adae89e9ecfee7af5ea79010325b2
TomNordesen <joachimflottorp@gmail.com>
2021-10-18 20:33:03 +0200

    Nice Commit! :)

--- commands/github.js
+++ commands/github.js
@@ -1,3 +1,5 @@
+const client = require("./../bot").client
+
 module.exports = {
     name: "github",
     ping: true,
@@ -5,7 +7,8 @@ module.exports = {
     perm: 100,
     execute: async (channel, user, input, perm) => {
         try {
-            return "https://github.com/JoachimFlottorp/Melonbot";
+            client.say(channel, "https://github.com/JoachimFlottorp/Melonbot");
+            return "";
         } catch (err) {
             console.log(err);
             return `Madge , bad command, you almost killed me! Notifying owner FeelsWeirdMan`
