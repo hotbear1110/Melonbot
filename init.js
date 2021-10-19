22cc4b98bb8bac6cd480f13482354de2b8dc072b
TomNordesen <joachimflottorp@gmail.com>
2021-10-19 19:38:15 +0200

    Nice Commit! :)

--- init.js
+++ init.js
@@ -9,7 +9,7 @@ global.ROOT = path.resolve(__dirname)
 
 // Could be wrong umask, no clue.
 // eslint-disable-next-line no-octal
-fs.mkdir(`${ROOT}/stats`, parseInt(022, 8), err => {
+fs.mkdir(`${ROOT}/stats`, parseInt(0777, 8), err => {
     if (err) {
         if (err.code === "EEXIST") return;
         console.log(err)
