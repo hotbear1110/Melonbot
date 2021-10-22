a1d1494a9d06c0b4866c0e51050ee087a97dc49b
TomNordesen <joachimflottorp@gmail.com>
2021-10-22 12:21:24 +0200

    Nice Commit! :)

--- website/routes/bot.js
+++ website/routes/bot.js
@@ -15,64 +15,82 @@ router
     // #[TODO]: Does not support switching years through the ui at the moment.
     // #[TODO]: Sometimes it just fails. dayStat returns undefined, no idea why. Even though the file very clearly exists..
 
-    let stats = "";
-    let single = req.query.channel === undefined ? false : true 
-    let year = req.query.year === undefined ? date.getFullYear() : req.query.year;
-    let month = req.query.month === undefined ? (date.getMonth() + 1) : req.query.month;
-    let day = req.query.day === undefined ? (date.getDate()) : req.query.day;
-    let dayStat;
+    try {
+        let stats = "";
+        let single = req.query.channel === undefined ? false : true 
+        let year = req.query.year === undefined ? Number(date.getFullYear()) : Number(req.query.year);
+        let month = req.query.month === undefined ? Number((date.getMonth()) + 1) : Number(req.query.month);
+        let day = req.query.day === undefined ? Number(date.getDate()) : Number(req.query.day);
+        let dayStat;
 
-    // Probably good enough 4Head
-    if (month >= 13) {
-        month = 12
-    }
+        // Probably good enough 4Head
+        if (month >= 13) {
+            month = 12
+        }
 
-    if (month <= 0) {
-        month = 1;
-    }
+        if (month <= 0) {
+            month = 1;
+        }
 
-    if (!single) {
-        stats = await tools.query("SELECT * FROM channel_stats");
-    } else {
-        stats = await tools.query("SELECT * FROM channel_stats WHERE Channel = ?", req.query.channel)
-        // Get the stats from files. Maybe we should not store this in files. Unsure.
-        const statFiles = requireDir("./../../stats");
+        if (!single) {
+            stats = await tools.query("SELECT * FROM channel_stats");
+        } else {
+            stats = await tools.query("SELECT * FROM channel_stats WHERE Channel = ?", req.query.channel)
+            // Get the stats from files. Maybe we should not store this in files. Unsure.
+            const statFiles = requireDir("./../../stats");
 
-        // Key = file
-        // Value = every stat per file [Every Channel]
-        for (const [key, value] of Object.entries(statFiles)) {
-            const a = key.split("-")
-            const fileYear = a[0];
-            const fileMonth = a[1];
-            const fileDay = a[2];
-            // Get the correct file for the specified date.
-            if (fileYear === year && fileMonth === month && fileDay === day) {
-                for (var property of value) {
-                    if (property.channel === req.query.channel) {
-                        dayStat = property
-                        delete dayStat['channel']
-                        break;
+            // Key = file
+            // Value = every stat per file [Every Channel]
+            for (const [key, value] of Object.entries(statFiles)) {
+                const a = key.split("-")
+                const fileYear = Number(a[0]);
+                const fileMonth = Number(a[1]);
+                const fileDay = Number(a[2]);
+                // Get the correct file for the specified date.
+                if (fileYear === year && fileMonth === month && fileDay === day) {
+                    for (var property of value) {
+                        // Adding two console logs seems to fix it on development box. But this probably does not fix it...
+                        console.log(value)
+                        console.log(property)
+                        if (property.channel === req.query.channel) {
+                            dayStat = property
+                            delete dayStat['channel']
+                            break;
+                        }
                     }
                 }
             }
+            console.log(dayStat)
         }
-        console.log(dayStat)
-    }
-    console.time('TimeRender')
+        console.time('TimeRender')
 
-    res.render('stats', {stats: stats, title: "Stats", specific: single, year: year, month: Number(month), day: Number(day), dayStat: dayStat}, function(err, html) {
-        if (err) return console.log('Render error: ', err);
-        res.send(html);
-        console.timeEnd('TimeRender')
-    })
-        
+        res.render('stats', {stats: stats, 
+                            title: "Stats", 
+                            specific: single, 
+                            year: year, 
+                            month: Number(month), 
+                            day: Number(day), 
+                            dayStat: dayStat}, 
+            function(err, html) {
+            if (err) return console.log('Render error: ', err);
+            res.send(html);
+            console.timeEnd('TimeRender')
+        })
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+        return;
+    }
 })
 
 // /Bot/Login
 router
     .route("/login")
     .get(async function(req, res) {
-        res.render('login', {title: "Login", server: creds.SERVER, LoggedIn: req.query.loggedIn}, function(err, html) {
+        res.render('login', {title: "Login", 
+                            server: creds.SERVER, 
+                            LoggedIn: req.query.loggedIn}, 
+            function(err, html) {
             if (err) return console.log('Render error: ', err);
             res.send(html);
         })
@@ -95,7 +113,9 @@ router
     .get(async function(req, res) {
         const commands = await tools.query("SELECT * FROM commands");
 
-        res.render('commands', { commands: commands, title: "Commands"}, function(err, html) {
+        res.render('commands', { commands: commands, 
+                                title: "Commands"}, 
+            function(err, html) {
             if (err) return console.log('Render error: ', err);
             res.send(html);
         });
