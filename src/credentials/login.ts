3c983a5309a44c40f931924bc71061af99e2c64e
Joachim Flottorp <Joachimflottorp@gmail.com>
2021-10-29 10:09:05 +0200

    sdaidjaosdjasdsdaidjaosdjasd

---
+++ src/credentials/login.ts
@@ -0,0 +1,64 @@
+import * as creds from './config.js';
+import mysql from 'mysql';
+import * as tools from './../tools/tools.js'
+export const con: mysql.Connection = mysql.createConnection({
+    host: creds.MYSQL_HOST,
+    user: creds.MYSQL_USER,
+    password: creds.MYSQL_PASSWORD,
+    database: creds.MYSQL_DATABASE
+});
+
+con.on('error', (err) => {
+    if (err.fatal) {
+        con.destroy();
+    }
+    throw err;
+});
+
+// Async function, keep pinging the sql server to keep it alive.
+export const pingDatabase = async () => {
+    setInterval(function() {
+        tools.query("SELECT 1");
+    }, 60000)
+}
+
+function getChannels(): Promise<Array<> {
+    new Promise((Resolve, Reject) => {
+        con.query('SELECT * FROM channels', (err, results) => {
+            if (err) {
+                Reject(err);
+            } else {
+                Resolve(results);
+            }
+        });
+    })
+});
+
+const channelOptions: string[] = []
+async function res() {
+    const channelList: Array<string> = []
+	channelList.push(await getChannels());
+	return await channelList[0].forEach(
+        i => {
+            channelOptions.push(i.channel_name)
+            tools.query("INSERT IGNORE INTO channel_stats (Channel) VALUES (?)", [i.channel_name])
+        } 
+    )
+}
+res()
+
+export const options = {
+    options: {
+        debug: true,
+        joinInterval: 300,
+    },
+
+    identity: {
+        username: creds.USERNAME,
+        password: creds.OAUTH
+    },
+    connection: {
+        port: 80
+    },
+    channels: channelOptions,
+}
